ed38d377d049efd8c7b1c344e024b1a6fb5c5f65
stefan-rajilic <stefan.rajilic@vitave.com>
2024-06-19 12:33:54 +0200

    Příprava na aktualizaci Angularu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NavbarLoginComponent } from './components/navbar-login/navbar-login.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    NavbarLoginComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
